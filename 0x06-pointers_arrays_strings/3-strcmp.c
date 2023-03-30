a6dea5c26a9f89f7c0c9c7ae5b02a0810903a4fd
JustusChesaro <justusck1@gmail.com>
2023-03-30 07:13:48 -0700

    corrected no3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,23 @@
-ls
 #include "main.h"
+
 /**
-* _strcmp - compares two strings
-*@s1: string 1
-*@s2: string 2
-*Return: the value compared
-*/
+ * _strcmp - compare
+ * @s1 : pointerto char params
+ * @s2 : pointer to char params
+ * Return: *dest
+ */
+
 int _strcmp(char *s1, char *s2)
 {
-int n;
+	int i;
+	int R;
 
-for (n = 0; s1[n] != '\0'; n++)
-{
-if (s1[n] > s2[n])
-return (s1[n] - s2[n]);
-if (s2[n] > s1[n])
-return (s1[n] - s2[n]);
-}
-return (0);
+	i = 0;
+
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	R = s1[i] - s2[i];
+	return (R);
 }
