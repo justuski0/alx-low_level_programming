7275a31c6f6ac83390393bede915e484b6a08496
JustusChesaro <justusck1@gmail.com>
2023-04-10 05:51:00 -0700

    mandatory tasks done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+	
+	return (0);
+}
